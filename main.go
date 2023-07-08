2af4884bd4773938c281d5182ea48ec1df061c39
ethan-stone <ethan.stone9352@gmail.com>
2023-07-08 11:17:32 -0400

    setup fiber project

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/hello/:name", func(c *fiber.Ctx) error {
+		name := c.Params("name")
+		return c.SendString("Hello " + name)
+	})
+
+	app.Listen(":3000")
+}
